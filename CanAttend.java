d09f55dc28c7bb178722e16baeb019a0f8fbb4f2
haleyprindle <haley@prindle.net>
2026-02-10 22:39:10 +0000

    done

--- CanAttend.java
+++ CanAttend.java
@@ -5,43 +5,41 @@ import java.util.ArrayList;
 
 public class CanAttend {
 
-	//precondition: meetings.getEnd() > meetings.getStart(); 
-	//postcondition: returns if both meetings can or can't be attended. Answers if the meetings overlap.
+	//precondition: the starting meeting interval is less than the ending meeting interval. 
+	//Also, neither intervals can be null.
+	//postcondition: returns if the meeting in the array list meetings can or can't be attended
+	//Answers if the meetings overlap.
 	public static boolean canAttend(ArrayList<MeetingInterval> meetings) {
 		//your implementation here
         
-            ArrayList<MeetingInterval> meet = new ArrayList<MeetingInterval>();
-           for (int i =0; i< meetings.length; i++){
-            for (int j = i+1; j<meetings.length; j++){
-            meet.add( new MeetingInterval (meetings[i], meetings[j]));
+
+           for (int i =0; i< meetings.size(); i++){
+            for (int j = i+1; j<meetings.size(); j++){
+           
+			MeetingInterval m1 = meetings.get(i);
+            MeetingInterval m2 = meetings.get(j);
+			if (m1.getStart() < m2.getEnd() && m2.getStart() < m1.getEnd()) {
+                    return false; // Overlap detected
             }
+			
                 
           }
+		}
          
-        for(MeetingInterval meetings: meeting){
-            int start = meetings.getStart();
-            int end = meetings.getEnd();
-            
-         if ((meetings.getStart()<start && meetings.getEnd()<end && meetings.getEnd()<start||meetings.getStart()>start && meetings.getEnd()>end && meetings.getStart()>end))
-            {
-                return true;
-            }    
-        }
+        
+
+        
         
 
 		System.out.println(meetings);
-		return false; //replace me with actual logic please!
+		return true; //replace me with actual logic please!
 	}
 	public static void main(String[] args) {
 		ArrayList<MeetingInterval> meet = new ArrayList<MeetingInterval>();
 		for(int i =0; i<10; i++) {
 			int start = (int)(Math.random()*2399);
 			int end = start + ((int)(Math.random()*(2399-start)));
-
-
 			meet.add(new MeetingInterval(start, end));
-
-
 
 		}
 		System.out.println(canAttend(meet) ? "There is no conflict with attending "+meet+" meetings":"Can't attend "+meet+" meetings due to conflict");
@@ -50,6 +48,8 @@ public class CanAttend {
         meet.clear();
         meet.add(new MeetingInterval(0,20));
         meet.add(new MeetingInterval(60,100));
+		System.out.println(canAttend(meet) ? "There is no conflict with attending "+meet+" meetings":"Can't attend "+meet+" meetings due to conflict");
+
 	}
 }
 
